8d34bba1ebfada97989ae64b98596d708bda371a
Pao <pzsigua@gmail.com>
2021-11-04 15:56:09 -0700

    update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from bson.objectid import ObjectId
 import os
 
 host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Playlister')
-client = MongoClient(host=host)
+client = MongoClient(host=f'{host}?retryWrites=false')
 db = client.get_default_database()
 playlists = db.playlists
 
@@ -90,4 +90,4 @@ def playlists_delete(playlist_id):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
